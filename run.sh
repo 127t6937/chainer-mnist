59caf566cda4c285c4e2e2efbeb6cfc130d6f2bb
aidiary <f2forest@gmail.com>
2017-03-15 11:43:10 +0900

    Modify to use gpu

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 for op in SGD MomentumSGD NesterovAG AdaGrad AdaDelta RMSprop Adam; do
-    python train_mnist.py --gpu -1 --optimizer $op --out result_$op
+    python train_mnist.py --gpu 0 --optimizer $op --out result_$op
 done
