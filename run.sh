a242432ca92038ad592e10f7c70e66fdee808f0d
aidiary <f2forest@gmail.com>
2017-03-15 14:19:16 +0900

    Comment in

--- run.sh
+++ run.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
 
-# # optimizer test
-# for op in SGD MomentumSGD NesterovAG AdaGrad AdaDelta RMSprop Adam; do
-#     python train_mnist.py --gpu 0 --optimizer $op --out result_$op
-# done
+# optimizer test
+for op in SGD MomentumSGD NesterovAG AdaGrad AdaDelta RMSprop Adam; do
+    python train_mnist.py --gpu 0 --optimizer $op --out result_$op
+done
 
-# # hidden unit test
-# for unit in 32 64 128 256 512 1024 2048 4096; do
-#     python train_mnist.py --gpu 0 --unit $unit --out result_unit_$unit
-# done
+# hidden unit test
+for unit in 32 64 128 256 512 1024 2048 4096; do
+    python train_mnist.py --gpu 0 --unit $unit --out result_unit_$unit
+done
 
-# for model in MLP1 MLP2 MLP3 MLP4 MLP5; do
-#     python train_mnist.py --gpu 0 --model $model --out result_$model
-# done
+# hidden layer test
+for model in MLP1 MLP2 MLP3 MLP4 MLP5; do
+    python train_mnist.py --gpu 0 --model $model --out result_$model
+done
 
-# for activation in sigmoid tanh relu leaky_relu elu; do
-#     python train_mnist.py --gpu 0 --activation $activation --out result_$activation
-# done
+# activation test
+for activation in sigmoid tanh relu leaky_relu elu; do
+    python train_mnist.py --gpu 0 --activation $activation --out result_$activation
+done
 
+# cnn test
 for model in CNN1 CNN2 CNN3; do
     python train_mnist.py --gpu 0 --model $model --out result_$model
 done
